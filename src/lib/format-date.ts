fdfbde0e3d4c559c6e9fed8001b8974cc9aaf798
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-12-02 21:51:25 +0900

    調整

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,7 +1,15 @@
-export const formatDate = (dateString: string): string => {
+export const formatDate = (dateString: string, type: "normal" | "jsonld" = "normal"): string => {
 	const date = new Date(new Date(dateString).toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" }));
 	const year = String(date.getFullYear());
 	const month = String(date.getMonth() + 1);
 	const day = String(date.getDate());
+	const hours = String(date.getHours());
+	const minutes = String(date.getMinutes());
+	const secounds = String(date.getSeconds());
+
+	if (type === "jsonld") {
+		return `${year}-${("00" + month).slice(-2)}-${("00" + day).slice(-2)}T${("00" + hours).slice(-2)}:${("00" + minutes).slice(-2)}:${("00" + secounds).slice(-2)}+09:00`;
+	}
+
 	return `${year}/${month}/${day}`;
 };
