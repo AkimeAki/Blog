b7368e93155d508ede5f646df148837b66e2bd04
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-03-27 20:21:49 +0900

    デザイン刷新

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,4 +1,4 @@
-export const formatDate = (dateString: string, type: "normal" | "jsonld" = "normal"): string => {
+export const formatDate = (dateString: string) => {
 	const date = new Date(new Date(dateString).toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" }));
 	const year = String(date.getFullYear());
 	const month = String(date.getMonth() + 1);
@@ -6,10 +6,36 @@ export const formatDate = (dateString: string, type: "normal" | "jsonld" = "norm
 	const hours = String(date.getHours());
 	const minutes = String(date.getMinutes());
 	const secounds = String(date.getSeconds());
+	const dayOfWeek = date.getDay();
 
-	if (type === "jsonld") {
-		return `${year}-${("00" + month).slice(-2)}-${("00" + day).slice(-2)}T${("00" + hours).slice(-2)}:${("00" + minutes).slice(-2)}:${("00" + secounds).slice(-2)}+09:00`;
+	return {
+		year: year,
+		month: ("00" + month).slice(-2),
+		date: ("00" + day).slice(-2),
+		day: dayOfWeek,
+		hour: ("00" + hours).slice(-2),
+		minute: ("00" + minutes).slice(-2),
+		secound: ("00" + secounds).slice(-2)
+	};
+};
+
+export const getEmojiOfWeek = (dayOfWeek: number) => {
+	switch (dayOfWeek) {
+		case 0:
+			return "☀️";
+		case 1:
+			return "🌙";
+		case 2:
+			return "🔥";
+		case 3:
+			return "💧";
+		case 4:
+			return "🌳";
+		case 5:
+			return "💰";
+		case 6:
+			return "🪐";
+		default:
+			return "🌈";
 	}
-
-	return `${year}/${month}/${day}`;
 };
